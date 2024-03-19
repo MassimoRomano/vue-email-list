a5011407c143e5f2e2a9bf29cc9a3d587259c5e2
MassimoRomano <romano93m@gmail.com>
2024-03-19 16:00:34 +0100

    add mouted in js and v-for in html

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,21 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            emails:[]
             
         }
     },
+
+    mounted() {
+        for(let i = 0; i < 10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(
+                (email) => {
+                    console.log(email.data.response);
+                    this.emails.push(email.data.response)
+                }
+            )
+        }
+
+    },
+
 }).mount('#app')
